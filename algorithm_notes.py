a1a8ec20bcfd5f5d05ec9cd79e221c1c731a2d82
kp1129 <katya.p.1129@gmail.com>
2020-07-20 11:50:40 -0700

    update my notes

--- algorithm_notes.py
+++ algorithm_notes.py
@@ -3,6 +3,7 @@ If you are looking through unordered data, there's
 unfortunately nothing better than looking at each 
 item to see if it's what you're looking for. 
 So if you can, sort first!
+O(n)
 
 """
 
@@ -15,6 +16,34 @@ def linear_search(arr, target):
 
 # https://visualgo.net/bn/sorting    
 
+"""
+If you can eliminate half the possibilities each round,
+like in binary search, big o improves to O(log n)
+
+"""
+def find_value_binary(arr, value):
+    first = 0
+    last = (len(arr) - 1)
+
+    found = False
+
+    while first <= last and not found:
+        # find middle
+        middle = (first + last) // 2
+
+        if arr[middle] == value:
+            found = True
+
+        else:
+            if value < arr[middle]:
+                # search the lower half
+                last = middle - 1 
+            else:
+                # search the upper half
+                first = middle + 1
+
+    return found      
+          
 
 """
 planning session for insertion sort
@@ -49,3 +78,5 @@ def insertion_sort(input_list):
         input_list[j] = current
 
     return input_list
+
+print(insertion_sort([9,8,7,6,5,4,3,2,1]))    
